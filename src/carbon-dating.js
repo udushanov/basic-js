5db12a1d3bd4aa794cd8ab04773b851678f8a8c4
Ulugbek <u.dushanov@gmail.com>
2022-04-22 13:14:43 +0500

    fix: carbon dating task

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -17,9 +17,16 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function dateSample( sampleActivity ) {
+  const k = 0.693 / HALF_LIFE_PERIOD
+  if (typeof sampleActivity === 'string') {
+    const s = parseFloat(sampleActivity)
+    if (s == false) {
+      return false
+    } else if (s <= MODERN_ACTIVITY && s > 0) {
+      return Math.ceil(Math.log(MODERN_ACTIVITY/s)/k)
+    } else return false
+  } else return false
 }
 
 module.exports = {
