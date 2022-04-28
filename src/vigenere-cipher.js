c12917f729b4b74e0ca18939739b44415fc731d5
Ulugbek <u.dushanov@gmail.com>
2022-04-28 11:59:54 +0500

    fix: vingere-cipher task

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,13 +20,55 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor(isDirect = true) {
+    this.isDirect = isDirect;
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  encrypt(message, key) {
+    if (!message || !key) throw new Error('Incorrect arguments!');
+    let res = [];
+    let spaces = 0;
+    message = message.toUpperCase().split('');
+    key = message.map((el, idx) => {
+      if (el === ' ') {
+        spaces++;
+        return el;
+      } else {
+        return key.toUpperCase()[(idx - spaces) % key.length]
+      }
+    });
+
+    for (let i = 0; i < message.length; i++) {
+      if (message[i].charCodeAt(0) >= 65 && message[i].charCodeAt(0) <= 90) {
+        let charCode = message[i].charCodeAt(0) - 65 + (key[i].charCodeAt(0) - 65);
+        message[i] = String.fromCharCode(charCode % 26 + 65);
+      }
+      res.push(message[i]);
+    }
+    return this.isDirect ? res.join('') : res.reverse().join('');
+  }
+  decrypt(message, key) {
+    if (!message || !key) throw new Error('Incorrect arguments!');
+    let res = [];
+    let spaces = 0;
+    message = message.toUpperCase().split('');
+    key = message.map((el, idx) => {
+      if (el === ' ') {
+        spaces++;
+        return el;
+      } else {
+        return key.toUpperCase()[(idx - spaces) % key.length]
+      }
+    });
+
+    for (let i = 0; i < message.length; i++) {
+      if (message[i].charCodeAt(0) >= 65 && message[i].charCodeAt(0) <= 90) {
+        let charCode = message[i].charCodeAt(0) + 65 - (key[i].charCodeAt(0) - 65);
+        message[i] = String.fromCharCode(charCode % 26 + 65);
+      }
+      res.push(message[i]);
+    }
+    return this.isDirect ? res.join('') : res.reverse().join('');
   }
 }
 
