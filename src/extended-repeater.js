3a122f19d1a288ed1d06ce8d737676b8ad3008e3
Ulugbek <u.dushanov@gmail.com>
2022-04-28 11:25:47 +0500

    fix: extended-repeater task

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -15,10 +15,18 @@ const { NotImplementedError } = require('../extensions/index.js');
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function repeater(str, {
+  repeatTimes = 1,
+  separator = "+",
+  addition = "",
+  additionRepeatTimes = 1,
+  additionSeparator = "|"
+}) {
+  const _addition = new Array(additionRepeatTimes).fill(String(addition)).join(additionSeparator);
+
+  return new Array(repeatTimes).fill(String(str) + _addition).join(separator);
 }
+
 
 module.exports = {
   repeater
